2be98eb37e06f5291cc485075186a10aa70d0107
Fred Blaise <fred.blaise@protonmail.com>
2022-10-28 17:39:37 +0200

    fix wrong class names

--- vbusnet_solar_thermal.py
+++ vbusnet_solar_thermal.py
@@ -52,7 +52,7 @@ class VbusnetKm2Data(hass.Hass):
         "state": panel_sensor_temp,
         "attributes": {
             "unit_of_measurement": "°C",
-            "state_class": "temperature"
+            "device_class": "temperature"
             }
         }
     reservoir_temp = data['headersets'][0]['packets'][0]['field_values'][1]['raw_value']
@@ -60,36 +60,40 @@ class VbusnetKm2Data(hass.Hass):
         "state": reservoir_temp,
         "attributes": {
             "unit_of_measurement": "°C",
-            "state_class": "temperature"
-            }
+            "device_class": "temperature"
         }
+    }
 
     pump_speed_relay = data['headersets'][0]['packets'][0]['field_values'][4]['raw_value']
     data_dict[pump_speed_relay_url] = {
         "state": pump_speed_relay,
         "attributes": {
             "unit_of_measurement": "%",
-            }
-        }
+         }
+    }
     
     panel_status_display = data['headersets'][0]['packets'][0]['field_values'][5]['raw_value']
     data_dict[panel_status_display_url] = {
-        "state": panel_status_display,
-        }
+       "state": panel_status_display,
+    }
     
     operating_hours_relay = data['headersets'][0]['packets'][0]['field_values'][6]['raw_value']
     data_dict[operating_hours_relay_url] = {
         "state": operating_hours_relay,
-        }
+        "attributes": {
+            "unit_of_measurement": "h",
+         }
+    }
     
     heat_quantity_total = data['headersets'][0]['packets'][0]['field_values'][7]['raw_value']
     data_dict[heat_quantity_total_url] = {
         "state": heat_quantity_total,
         "attributes": {
             "unit_of_measurement": "Wh",
-            "state_class": "energy"
-            }
+            "device_class": "energy",
+	    "state_class": "total_increasing"
         }
+    }
     
     self._post_to_entity(data_dict)
       
